766d9363cf684e11c5be67e32f793206c969d46c
LaukinJ <55482563+LaukinJ@users.noreply.github.com>
2019-09-18 16:08:39 +0800

    Add files via upload
    创建了head，foot

---
+++ 项目/头部/js/header.js
@@ -0,0 +1,8 @@
+$(function () {
+    $(".hide").mouseenter(function () {
+        $(".myaccount").css("display", "block");
+    })
+    $(".hide").mouseleave(function () {
+        $(".myaccount").css("display", "none");
+    })
+})
